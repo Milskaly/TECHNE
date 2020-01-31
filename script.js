a3e19adf240c6cc56fdabe8342482d5da3fdeeab
Emilia Kalyvides <emiliakalyvides@Emilias-iMac.connect>
2020-01-31 21:04:28 +0000

    new pages added

---
+++ script.js
@@ -0,0 +1,4 @@
+// removeCartButtonItems stores all the variables with the class name 'button-shop'
+
+let removeCartButtonItems = document.getElementsByClassName("button-shop");
+console.log(removeCartButtonItems);
